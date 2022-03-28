f3999a200094559a141009baabbd7832d53cec1d
pepperedwhale-active <suswaram.aniruda@gmail.com>
2022-03-28 23:09:51 +0530

    code reduction using functions

--- moves.h
+++ moves.h
@@ -4,37 +4,28 @@
 //movement of indexes to perform a clockwise rotation of the right face:
 //2->37,18->23,32->47,4->35,17->20,7->32,10->2,16->18,12->4,19->17,15->7,21->16,42->10,44->12,22->19,
 //47->15,23->21,37->42
+
+void switch_R(int cubestate[48], int index1, int index2, int index3, int index4)
+{
+    int c = cubestate[index3];
+    cubestate[index3] = cubestate[index2];
+    cubestate[index2] = cubestate[index1];
+    cubestate[index1] = cubestate[index4];
+    cubestate[index4] = c;
+}
+
 void move_R(int cubestate[48])
 {
-    int c = cubestate[42];
-    cubestate[42] = cubestate[37];
-    cubestate[37] = cubestate[2];
-    cubestate[2] = cubestate[10];
-    cubestate[10] = c;
+    switch_R(cubestate,2,37,42,10);
 
-    int d = cubestate[47];
-    cubestate[47] = cubestate[32];
-    cubestate[32] = cubestate[7];
-    cubestate[7] = cubestate[15];
-    cubestate[15] = d;
+    switch_R(cubestate,7,32,47,15);
 
-    int e = cubestate[23];
-    cubestate[23] = cubestate[18];
-    cubestate[18] = cubestate[16];
-    cubestate[16] = cubestate[21];
-    cubestate[21] = e;
+    switch_R(cubestate,16,18,23,21);
 
-    int f = cubestate[44];
-    cubestate[44] = cubestate[35];
-    cubestate[35] = cubestate[4];
-    cubestate[4] = cubestate[12];
-    cubestate[12] = f;
-    
-    int g = cubestate[22];
-    cubestate[22] = cubestate[20];
-    cubestate[20] = cubestate[17];
-    cubestate[17] = cubestate[19];
-    cubestate[19] = g;
-    }
+    switch_R(cubestate,4,35,44,12);
+
+    switch_R(cubestate,17,20,22,19)
+
+}
 
 #endif

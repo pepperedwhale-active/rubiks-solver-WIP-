a35a417e2f061cd85aa6f67d62ddf55056454c8a
pepperedwhale-active <suswaram.aniruda@gmail.com>
2022-03-29 12:05:41 +0530

    finished all R type turns and moved color defs and added enums to rep moves

--- moves.h
+++ moves.h
@@ -1,5 +1,11 @@
 #ifndef MOVES_H
 #define MOVES_H
+enum{
+    WHITE = 1, GREEN, RED, ORANGE, BLUE, YELLOW
+};
+enum{
+    R, R_Prime, R_2
+};
 //white is held on top, green in the front and red on the right.
 //an R move is defined as the clockwise rotation of the squares present in the indexes:
 //2,37,42,10
@@ -35,15 +41,23 @@ void switch_Case(int cubestate[48], int index1, int index2, int index3, int inde
         //double rotation
         case 'T':
         {
+            cubestate[index1] = cubestate[index1] + cubestate[index3];
+            cubestate[index3] = cubestate[index1] - cubestate[index3];
+            cubestate[index1] = cubestate[index1] - cubestate[index3];
+
+            cubestate[index2] = cubestate[index4] + cubestate[index2];
+            cubestate[index4] = cubestate[index2] - cubestate[index4];
+            cubestate[index2] = cubestate[index2] - cubestate[index4];
 
         }
+        break;
     }
 }
 
-void moveFace(int cubestate[48], char move)
+void moveFace(int cubestate[48], int move)
 {
     switch(move){
-        case 'R':{
+        case R:{
             switch_Case(cubestate,2,37,42,10,'C');
             switch_Case(cubestate,7,32,47,15,'C');
             switch_Case(cubestate,16,18,23,21,'C');
@@ -51,7 +65,7 @@ void moveFace(int cubestate[48], char move)
             switch_Case(cubestate,17,20,22,19,'C');
         }
         break;
-        case 'S':{//S denotes R'
+        case R_Prime:{//S denotes R'
             switch_Case(cubestate,2,37,42,10,'A');
             switch_Case(cubestate,7,32,47,15,'A');
             switch_Case(cubestate,16,18,23,21,'A');
@@ -59,6 +73,14 @@ void moveFace(int cubestate[48], char move)
             switch_Case(cubestate,17,20,22,19,'A');
         }   
         break;
+        case R_2:{
+            switch_Case(cubestate,2,47,42,7,'T');
+            switch_Case(cubestate,32,37,15,10,'T');
+            switch_Case(cubestate,16,18,23,21,'T');
+            switch_Case(cubestate,4,35,44,12,'T');
+            switch_Case(cubestate,17,20,22,19,'T');
+        }
+
     }
 }
 
--- solve.h
+++ solve.h
@@ -1,13 +1,8 @@
 #ifndef INTIALIZE_H
 #define INTIALIZE_H
 #include "moves.h"
-enum{
-    WHITE = 1, GREEN, RED, ORANGE, BLUE, YELLOW
-};
-
 void solve(int cubearray[48], char movelist[21])
 {
-    
 }
 
 #endif
